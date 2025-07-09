a22628a7034270361f77fb4946e2fff8e5a23b86
Albert NIYONSENGA <a.niyonseng@alustudent.com>
2025-07-09 09:06:52 +0000

    Documentation added :ghost:

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-
 """
+Reddit Suscriber count module
 Working with Reddit API to counts subreddit subscribbers
 """
 import requests
@@ -8,8 +8,12 @@ import sys
 
 def number_of_subscribers(subreddit):
     """
-    Queries the Reddit API and returns the number of subscribers for a given subreddit.
-    If the subreddit is invalid, returns 0.
+    Queries the Reddit API and returns the number of subscribers for a given subreddit. 
+    Args:
+        subreddit(str): Name for the subreddit to query
+
+    Returns:
+        int: Number of subcribers(0 for invalid subreddits or errors).
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'Python:RedditAPI.Exercise:v1.0 (by /u/anonymous)'}
