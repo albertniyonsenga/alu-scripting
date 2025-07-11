ec652ac75f8ba54041054548e31877b55567105a
Albert NIYONSENGA <a.niyonseng@alustudent.com>
2025-07-11 07:48:28 +0000

    File updated

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -21,9 +21,3 @@ def number_of_subscribers(subreddit):
         return 0
     except requests.RequestException:
         return 0
-
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        print(number_of_subscribers(sys.argv[1]))
